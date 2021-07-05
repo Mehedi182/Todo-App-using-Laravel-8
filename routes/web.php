4a1d0ab833779a2472042094d9f580f06c08f171
mehedi182 <mehedihasan152201@gmail.com>
2021-07-05 13:04:21 +0600

    3rd commit

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
